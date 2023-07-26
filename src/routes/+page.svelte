8e7d6ee8293a634ccbc26b52d56e751d6468787c
Andy <onsubmit@outlook.com>
2023-07-25 21:18:29 -0700

    Add Vite/SvelteKit/Tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import '../app.css';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.blue.950);
+  }
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import postcss from './postcss.config.js';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  css: {
+    postcss
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
+});
